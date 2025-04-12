8084631d1670ef18124958815d61a7ffdf6335b3
Awais Akram <awaisakram182@gmail.com>
2025-04-12 13:02:25 +0500

    done

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
- base:"Sound-Etthics",
+ base:"/Sound-Etthics/",
   plugins: [react(),tailwindcss(),],
 })
